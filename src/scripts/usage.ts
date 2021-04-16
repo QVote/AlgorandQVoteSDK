1ed931e71b8211deb1a848aa45066148a8963359
Edoardo Pona <edoardo.pona@gmail.com>
2021-04-16 14:53:40 +0200

    working deploy create txn :fire: :zap: :zap:

---
+++ src/scripts/usage.ts
@@ -0,0 +1,150 @@
+import * as algosdk from "algosdk";
+import * as fs from "fs"
+import {compileProgram, createApp, waitForConfirmation} from "./utils" 
+import {approvalProgramSourceRefactored, approvalProgramSourceInitial, clearProgramSource} from "./contracts"
+import * as assert from "assert"
+
+const NULL_OPTION_SYM = "NULL_OPTION"  // NOTE: could store it already encoded 
+
+// demo account, will be replaced by wallet provided one 
+// const creatorMnemonic = 'couple zone group already phone alley mercy napkin rival one talk carbon useless road bag anger impact ski media scout dune wave original absent grape'; 
+const creatorMnemonic = "brush travel spot crumble network trigger kind pear depth warm dash assault poet jump frown aim embrace clog obtain simple six perfect junk abstract speak"
+
+const creatorAccount = algosdk.mnemonicToSecretKey(creatorMnemonic);
+const userMnemonic = "menu circle frozen typical radio cry various shrug chef dance inmate obtain trouble absent food bicycle twice bench undo rice snow click ability abstract mystery";
+
+const baseServer = 'https://testnet-algorand.api.purestake.io/ps2'
+const port = '';
+const token = {
+	'X-API-Key': process.env.PURESTAKE_ALGORAND_API
+}
+
+const algodClient = new algosdk.Algodv2(token, baseServer, port);
+function loadCompiledPrograms() : {approval: Uint8Array, clearState: Uint8Array}{
+	const approvalRead = fs.readFileSync("../ContractCode/quadratic_voting_approval.teal.tok", {encoding: "base64"})
+	const approvalProgram = new Uint8Array(Buffer.from(approvalRead, "base64"))
+	const clearRead = fs.readFileSync("../ContractCode/quadratic_voting_clear_state.teal.tok", {encoding: "base64"});
+	const clearProgram = new Uint8Array(Buffer.from(clearRead, "base64"))
+	return {approval: approvalProgram, clearState: clearProgram}
+}
+
+
+function encodeString(s: string) : Uint8Array {
+	return new Uint8Array(Buffer.from(s));
+}
+
+function encodeNumber(n: number){
+	return new Uint8Array([n]);
+}
+
+function pad(options: string[]) : string[] {
+	assert(options.length <= 5)
+	for (var i=0; options.length < 5; i++) {
+		options.push(NULL_OPTION_SYM); 
+	}
+	return options;
+}
+
+
+function buildQVoteDeployArgs(decisionName: string, options: string[],
+					  startTime: number, endTime: number, 
+					  assetId: number, assetCoefficient: number) : Uint8Array[]{
+
+	assert(options.length <= 5) 
+	const appArgs = [encodeString(decisionName)]
+						.concat(options.map(encodeString))
+						.concat([assetId, assetCoefficient, startTime, endTime].map(encodeNumber))
+
+	return appArgs;
+}
+
+/*
+ * groups the options in lists of length 5 and pads the last one 
+ */
+function groupOptions(options: string[]) : string[][] {
+	var out = [];
+	options.map((d, i) => {(i%5==0) && (out.push([])); out[out.length-1].push(d)})
+	out[out.length-1] = pad(out[out.length-1])
+	return out;
+}
+
+
+// NOTE: we need a client in the sdk. We can create one here that has no prvileges over the account (we only need it to read data from the api). 
+// The client that will sign transactions (injected by the wallet) should not be used in the sdk, or at least it should not be mandatory.
+/*
+ * Returns an unsigned transaction for deploying a QVote decision 
+ */
+async function buildDeployTxs(client: any, creatorAddress: string,
+							 startTime: number, registrationSeconds: number,
+							 assetId: number, assetCoefficient: number, 
+							 decisionName: string, options: string[]) : Promise<any[]>{
+								 
+	assert(options.length <= 61)    // won't fit in the contract otherwise 
+	var txs = []
+
+	// Application Creation tx 
+	const groupedOptions = groupOptions(options);
+	const {approval, clearState} = loadCompiledPrograms();
+    const onComplete = algosdk.OnApplicationComplete.NoOpOC;
+    const params = await client.getTransactionParams().do();
+
+	const localInts = 1;
+	const localBytes = 1;
+	const globalInts = options.length;   
+	const globalBytes = 3;
+	const endTime = startTime + registrationSeconds
+
+	const appArgs = buildQVoteDeployArgs(decisionName, groupedOptions[0], startTime, endTime, assetId, assetCoefficient);
+	const appCreateTx =  algosdk.makeApplicationCreateTxn(creatorAddress, params, onComplete, 
+											approval, clearState, 
+											localInts, localBytes, globalInts, globalBytes, 
+											appArgs); 
+	txs.push(appCreateTx)
+
+	// Add option txs 
+	groupedOptions.slice(1).map((opts) => )
+	return txs;
+
+}
+
+// TODO compute all transactions needed to deploy a whole election. 
+// In general the approach is we build transactions that then people sign themselves. All of them. So in creation of the application, even the ones 
+// needed to register voters. We return a list of transactions that have to be signed and sent. 
+
+(async () => {
+	try {
+		const startTime = Math.round(Date.now() / 1000)
+		const registrationSeconds = 300	
+		const assetId = 13164495;
+		const assetCoefficient = 2;
+		const decisionName = "muchdecision"
+		const options = ["first", "second", "third", "fourth", "fifth", "sixth", "seventh"]
+
+		const txs = await buildDeployTxs(algodClient, creatorAccount.addr, 
+										 startTime, registrationSeconds, 
+										 assetId, assetCoefficient, 
+										 decisionName, options);
+		const deployTx = txs[0]
+		const txId = deployTx.txID().toString();
+		console.log("built tx", txId);
+
+		let signedTxn = deployTx.signTxn(creatorAccount.sk);
+		console.log("Signed transaction with txID: %s", txId);
+
+		await algodClient.sendRawTransaction(signedTxn).do();
+		console.log("SENT")
+
+		await waitForConfirmation(algodClient, txId);
+
+		// display results
+		let transactionResponse = await algodClient.pendingTransactionInformation(txId).do();
+		let appId = transactionResponse['application-index']; 
+
+	   	// const appId = await deploy(creatorMnemonic); 
+		console.log(appId)
+	} catch (e) {
+		console.log(e)
+	}
+})(); 
+
+
--- src/scripts/utils.ts
+++ src/scripts/utils.ts
@@ -1,50 +1,337 @@
+import * as algosdk from "algosdk";
+import * as fs from "fs"
 
-export async function waitForConfirmation(algodclient, txId, timeout) {
-  // Wait until the transaction is confirmed or rejected, or until 'timeout'
-  // number of rounds have passed.
-  //     Args:
-  // txId(str): the transaction to wait for
-  // timeout(int): maximum number of rounds to wait
-  // Returns:
-  // pending transaction information, or throws an error if the transaction
-  // is not confirmed or rejected in the next timeout rounds
-  if (algodclient == null || txId == null || timeout < 0) {
-    throw new Error('Bad arguments.');
-  }
-  const status = await algodclient.status().do();
-  if (typeof status === 'undefined')
-    throw new Error('Unable to get node status');
-  const startround = status['last-round'] + 1;
-  let currentround = startround;
-
-  /* eslint-disable no-await-in-loop */
-  while (currentround < startround + timeout) {
-    const pendingInfo = await algodclient
-      .pendingTransactionInformation(txId)
-      .do();
-    if (pendingInfo !== undefined) {
-      if (
-        pendingInfo['confirmed-round'] !== null &&
-        pendingInfo['confirmed-round'] > 0
-      ) {
-        // Got the completed Transaction
-        return pendingInfo;
-      }
-
-      if (
-        pendingInfo['pool-error'] != null &&
-        pendingInfo['pool-error'].length > 0
-      ) {
-        // If there was a pool error, then the transaction has been rejected!
-        throw new Error(
-          `Transaction Rejected pool error${pendingInfo['pool-error']}`
-        );
-      }
-    }
-    await algodclient.statusAfterBlock(currentround).do();
-    currentround += 1;
-  }
-  /* eslint-enable no-await-in-loop */
-  throw new Error(`Transaction not confirmed after ${timeout} rounds!`);
+// helper function to compile program source  
+export async function compileProgram(client, programSource) {
+    let encoder = new TextEncoder();
+    let programBytes = encoder.encode(programSource);
+    let compileResponse = await client.compile(programBytes).do();
+    let compiledBytes = new Uint8Array(Buffer.from(compileResponse.result, "base64"));
+    return compiledBytes;
 }
 
+// helper function to await transaction confirmation
+// Function used to wait for a tx confirmation
+export const waitForConfirmation = async function (algodclient, txId) {
+    let status = (await algodclient.status().do());
+    let lastRound = status["last-round"];
+      while (true) {
+        const pendingInfo = await algodclient.pendingTransactionInformation(txId).do();
+        if (pendingInfo["confirmed-round"] !== null && pendingInfo["confirmed-round"] > 0) {
+          //Got the completed Transaction
+          console.log("Transaction " + txId + " confirmed in round " + pendingInfo["confirmed-round"]);
+          break;
+        }
+        lastRound++;
+        await algodclient.statusAfterBlock(lastRound).do();
+      }
+    };
+
+// create new application
+export async function createApp(client, creatorAccount, approvalProgram, clearProgram, localInts, localBytes, globalInts, globalBytes) {
+    // define sender as creator
+    const sender = creatorAccount.addr;
+
+    // declare onComplete as NoOp
+    const onComplete = algosdk.OnApplicationComplete.NoOpOC;
+
+	// get node suggested parameters
+    let params = await client.getTransactionParams().do();
+    // comment out the next two lines to use suggested fee
+    params.fee = 1000;
+    params.flatFee = true;
+
+    // create unsigned transaction
+    let txn = algosdk.makeApplicationCreateTxn(sender, params, onComplete, 
+                                            approvalProgram, clearProgram, 
+                                            localInts, localBytes, globalInts, globalBytes,);
+    let txId = txn.txID().toString();
+
+    // Sign the transaction
+    let signedTxn = txn.signTxn(creatorAccount.sk);
+    console.log("Signed transaction with txID: %s", txId);
+
+    // Submit the transaction
+    await client.sendRawTransaction(signedTxn).do();
+
+    // Wait for confirmation
+    await waitForConfirmation(client, txId);
+
+    // display results
+    let transactionResponse = await client.pendingTransactionInformation(txId).do();
+    let appId = transactionResponse['application-index'];
+    console.log("Created new app-id: ",appId);
+    return appId;
+}
+
+// optIn
+async function optInApp(client, account, index) {
+    // define sender
+    const sender = account.addr;
+
+	// get node suggested parameters
+    let params = await client.getTransactionParams().do();
+    // comment out the next two lines to use suggested fee
+    params.fee = 1000;
+    params.flatFee = true;
+
+    // create unsigned transaction
+    let txn = algosdk.makeApplicationOptInTxn(sender, params, index);
+    let txId = txn.txID().toString();
+
+    // Sign the transaction
+    let signedTxn = txn.signTxn(account.sk);
+    console.log("Signed transaction with txID: %s", txId);
+
+    // Submit the transaction
+    await client.sendRawTransaction(signedTxn).do();
+
+    // Wait for confirmation
+    await waitForConfirmation(client, txId);
+
+    // display results
+    let transactionResponse = await client.pendingTransactionInformation(txId).do();
+    console.log("Opted-in to app-id:",transactionResponse['txn']['txn']['apid'])
+}
+
+// call application 
+async function callApp(client, account, index, appArgs) {
+    // define sender
+    const sender = account.addr;
+
+    // get node suggested parameters
+    let params = await client.getTransactionParams().do();
+    // comment out the next two lines to use suggested fee
+    params.fee = 1000;
+    params.flatFee = true;
+
+    // create unsigned transaction
+    let txn = algosdk.makeApplicationNoOpTxn(sender, params, index, appArgs)
+    let txId = txn.txID().toString();
+
+    // Sign the transaction
+    let signedTxn = txn.signTxn(account.sk);
+    console.log("Signed transaction with txID: %s", txId);
+
+    // Submit the transaction
+    await client.sendRawTransaction(signedTxn).do();
+
+    // Wait for confirmation
+    await waitForConfirmation(client, txId);
+
+    // display results
+    let transactionResponse = await client.pendingTransactionInformation(txId).do();
+    console.log("Called app-id:",transactionResponse['txn']['txn']['apid'])
+    if (transactionResponse['global-state-delta'] !== undefined ) {
+        console.log("Global State updated:",transactionResponse['global-state-delta']);
+    }
+    if (transactionResponse['local-state-delta'] !== undefined ) {
+        console.log("Local State updated:",transactionResponse['local-state-delta']);
+    }
+}
+
+// read local state of application from user account
+async function readLocalState(client, account, index){
+    let accountInfoResponse = await client.accountInformation(account.addr).do();
+    for (let i = 0; i < accountInfoResponse['apps-local-state'].length; i++) { 
+        if (accountInfoResponse['apps-local-state'][i].id == index) {
+            console.log("User's local state:");
+            for (let n = 0; n < accountInfoResponse['apps-local-state'][i][`key-value`].length; n++) {
+                console.log(accountInfoResponse['apps-local-state'][i][`key-value`][n]);
+            }
+        }
+    }
+}
+
+// read global state of application
+async function readGlobalState(client, account, index){
+    let accountInfoResponse = await client.accountInformation(account.addr).do();
+    for (let i = 0; i < accountInfoResponse['created-apps'].length; i++) { 
+        if (accountInfoResponse['created-apps'][i].id == index) {
+            console.log("Application's global state:");
+            for (let n = 0; n < accountInfoResponse['created-apps'][i]['params']['global-state'].length; n++) {
+                console.log(accountInfoResponse['created-apps'][i]['params']['global-state'][n]);
+            }
+        }
+    }
+}
+
+async function updateApp(client, creatorAccount, index, approvalProgram, clearProgram) {
+    // define sender as creator
+    const sender = creatorAccount.addr;
+
+	// get node suggested parameters
+    let params = await client.getTransactionParams().do();
+    // comment out the next two lines to use suggested fee
+    params.fee = 1000;
+    params.flatFee = true;
+
+    // create unsigned transaction
+    let txn = algosdk.makeApplicationUpdateTxn(sender, params, index, approvalProgram, clearProgram);
+    let txId = txn.txID().toString();
+
+    // Sign the transaction
+    let signedTxn = txn.signTxn(creatorAccount.sk);
+    console.log("Signed transaction with txID: %s", txId);
+
+    // Submit the transaction
+    await client.sendRawTransaction(signedTxn).do();
+
+    // Wait for confirmation
+    await waitForConfirmation(client, txId);
+
+    // display results
+    let transactionResponse = await client.pendingTransactionInformation(txId).do();
+    let appId = transactionResponse['txn']['txn'].apid;
+    console.log("Updated app-id: ",appId);
+    return appId;
+}
+
+// close out from application 
+async function closeOutApp(client, account, index) {
+    // define sender
+    const sender = account.addr;
+
+    // get node suggested parameters
+    let params = await client.getTransactionParams().do();
+    // comment out the next two lines to use suggested fee
+    params.fee = 1000;
+    params.flatFee = true;
+
+    // create unsigned transaction
+    let txn = algosdk.makeApplicationCloseOutTxn(sender, params, index)
+    let txId = txn.txID().toString();
+
+    // Sign the transaction
+    let signedTxn = txn.signTxn(account.sk);
+    console.log("Signed transaction with txID: %s", txId);
+
+    // Submit the transaction
+    await client.sendRawTransaction(signedTxn).do();
+
+    // Wait for confirmation
+    await waitForConfirmation(client, txId);
+
+    // display results
+    let transactionResponse = await client.pendingTransactionInformation(txId).do();
+    console.log("Closed out from app-id:",transactionResponse['txn']['txn']['apid'])
+}
+
+async function deleteApp(client, creatorAccount, index) {
+    // define sender as creator
+    const sender = creatorAccount.addr;
+
+	// get node suggested parameters
+    let params = await client.getTransactionParams().do();
+    // comment out the next two lines to use suggested fee
+    params.fee = 1000;
+    params.flatFee = true;
+
+    // create unsigned transaction
+    let txn = algosdk.makeApplicationDeleteTxn(sender, params, index);
+    let txId = txn.txID().toString();
+
+    // Sign the transaction
+    let signedTxn = txn.signTxn(creatorAccount.sk);
+    console.log("Signed transaction with txID: %s", txId);
+
+    // Submit the transaction
+    await client.sendRawTransaction(signedTxn).do();
+
+    // Wait for confirmation
+    await waitForConfirmation(client, txId);
+
+    // display results
+    let transactionResponse = await client.pendingTransactionInformation(txId).do();
+    let appId = transactionResponse['txn']['txn'].apid;
+    console.log("Deleted app-id: ",appId);
+    return appId;
+}
+
+async function clearApp(client, account, index) {
+    // define sender as creator
+    const sender = account.addr;
+
+	// get node suggested parameters
+    let params = await client.getTransactionParams().do();
+    // comment out the next two lines to use suggested fee
+    params.fee = 1000;
+    params.flatFee = true;
+
+    // create unsigned transaction
+    let txn = algosdk.makeApplicationClearStateTxn(sender, params, index);
+    let txId = txn.txID().toString();
+
+    // Sign the transaction
+    let signedTxn = txn.signTxn(account.sk);
+    console.log("Signed transaction with txID: %s", txId);
+
+    // Submit the transaction
+    await client.sendRawTransaction(signedTxn).do();
+
+    // Wait for confirmation
+    await waitForConfirmation(client, txId);
+
+    // display results
+    let transactionResponse = await client.pendingTransactionInformation(txId).do();
+    let appId = transactionResponse['txn']['txn'].apid;
+    console.log("Cleared local state for app-id: ",appId);
+    return appId;
+}
+
+/*
+async function main() {
+    try {
+
+
+    // opt-in to application
+    await optInApp(algodClient, userAccount, appId);
+
+    // call application without arguments
+    await callApp(algodClient, userAccount, appId, undefined);
+
+    // read local state of application from user account
+    await readLocalState(algodClient, userAccount, appId);
+
+    // read global state of application
+    await readGlobalState(algodClient, creatorAccount, appId);
+
+    // update application
+    approvalProgram = await compileProgram(algodClient, approvalProgramSourceRefactored);
+    await updateApp(algodClient, creatorAccount, appId, approvalProgram, clearProgram);
+
+    // call application with arguments
+    let ts = new Date(new Date().toUTCString());
+    console.log(ts)
+    let appArgs = [];
+    appArgs.push(new Uint8Array(Buffer.from(ts)));
+    await callApp(algodClient, userAccount, appId, appArgs);
+
+    // read local state of application from user account
+    await readLocalState(algodClient, userAccount, appId);
+
+    // close-out from application
+    await closeOutApp(algodClient, userAccount, appId)
+
+    // opt-in again to application
+    await optInApp(algodClient, userAccount, appId)
+
+    // call application with arguments
+    await callApp(algodClient, userAccount, appId, appArgs)
+
+    // read local state of application from user account
+    await readLocalState(algodClient, userAccount, appId);
+
+    // delete application
+    await deleteApp(algodClient, creatorAccount, appId)
+
+    // clear application from user account
+    await clearApp(algodClient, userAccount, appId)
+    
+    }
+    catch (err){
+        console.log("err", err);  
+    }
+}
+
+main(); */ 
